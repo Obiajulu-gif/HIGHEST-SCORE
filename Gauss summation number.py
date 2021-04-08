956ce21ab7cb4d944cf9206a8144d96dc4e3cef5
Emmanuel Obiajulu Okoye <>
2021-04-08 18:33:19 +0000

    ADDING THE CODE IN GAUSS

---
+++ Gauss summation number.py
@@ -0,0 +1,7 @@
+total = 0
+# n will loop into number of times the range have
+# range (a, b-1) this means we are to add from the upper limit by one to get the exact value
+for n in range (1, 101):
+    total += n
+print (total)
+
